d92a13ba2c6c225794144fd9af83f8656c0fabd6
day0ff <day85@tut.by>
2019-07-08 00:43:45 +0300

    Init CroppedImage component.

--- joystick/src/app/App.js
+++ joystick/src/app/App.js
@@ -1,15 +1,15 @@
 import React, {Component, Fragment} from 'react';
 import {StyleSheet, View, Text, Image} from 'react-native';
 
-const img = require('../images/joystick-buttons-sprites.png');
+const JOYSTICK_BUTTONS_SPRITES = require('../images/joystick-buttons-sprites.png');
 
 const BUTTONS = {
     CIRCLE_ON: {
-        uri: require('../images/joystick-buttons-sprites.png'),
+        uri: JOYSTICK_BUTTONS_SPRITES,
         crop: {left: 512, top: 1024, width: 512, height: 512}
     },
     CIRCLE_OFF: {
-        uri: require('../images/joystick-buttons-sprites.png'),
+        uri: JOYSTICK_BUTTONS_SPRITES,
         crop: {left: 0, top: 1024, width: 512, height: 512}
     },
 };
@@ -19,8 +19,8 @@ class App extends Component {
         return (
             <Fragment>
                 <View>
-                    <Text>{img.toString()}</Text>
-                    <Image style={{width: 50, height: 100}} source={img}/>
+                    <Text>{JOYSTICK_BUTTONS_SPRITES.toString()}</Text>
+                    <Image style={{width: 50, height: 100}} source={JOYSTICK_BUTTONS_SPRITES}/>
                 </View>
             </Fragment>
         );
