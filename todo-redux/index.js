fc240c13dd88e7ba3a2949bbc96f687ef8ae94f4
dzianis_viarshadau <day85@tut.by>
2019-07-23 12:25:07 +0300

    Added activity indicator while data is loading.

--- todo-redux/index.js
+++ todo-redux/index.js
@@ -5,11 +5,15 @@ import { name as appName } from './app.json';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 
+import { View, ActivityIndicator } from 'react-native';
+
 import { store, persistor } from './src/store/store';
+
+const loading = () => (<View><ActivityIndicator size="large"/></View>);
 
 const RNRedux = () => (
     <Provider store={store}>
-        <PersistGate persistor={persistor} loading={null}>
+        <PersistGate persistor={persistor} loading={loading}>
             <App/>
         </PersistGate>
     </Provider>
