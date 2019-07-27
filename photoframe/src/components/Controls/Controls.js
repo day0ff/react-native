16c7507354cf5c49bd044b01df40a2a468fffaed
day0ff <day85@tut.by>
2019-07-27 19:43:51 +0300

    Implement clear picture action.

--- photoframe/src/components/Controls/Controls.js
+++ photoframe/src/components/Controls/Controls.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import { View, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 import eraser from '../../images/icons/eraser_all_active.png';
 import upload from '../../images/icons/upload_active.png';
 import save from '../../images/icons/save_active.png';
+import { PICTURE_ACTION } from '../../store/actions/picture-action';
+
+const {clearPicture} = PICTURE_ACTION;
 
 class Controls extends Component {
 
-    constructor() {
-        super();
-    }
+    clearPicture = () => {
+        const {columns, rows} = this.props;
+        this.props.clearPicture(columns, rows);
+    };
 
     render() {
         return (
             <View style={[this.props.style, styles.controls]}>
-                <TouchableOpacity>
+                <TouchableOpacity onPress={this.clearPicture}>
                     <Image source={eraser} style={[styles.common]}/>
                 </TouchableOpacity>
                 <TouchableOpacity>
@@ -38,12 +43,24 @@ const styles = StyleSheet.create({
         justifyContent: 'space-evenly',
         width: '100%',
         height: '100%',
-        backgroundColor:'black',
+        backgroundColor: 'black',
     },
     common: {
         width: 60,
         height: 60,
     },
 });
+const mapStateToProps = state => ({
+    columns: state.pictureReducer.columns,
+    rows: state.pictureReducer.rows,
+});
 
-export default Controls;
+const mapDispatchToProps = dispatch => {
+    return {
+        clearPicture: (columns, rows) => {
+            dispatch(clearPicture(columns, rows))
+        },
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Controls);
--- photoframe/src/store/actions/picture-action.js
+++ photoframe/src/store/actions/picture-action.js
@@ -5,11 +5,11 @@ const {CHANGE_PICTURE_ROWS, CHANGE_PICTURE_COLUMNS, CHANGE_PICTURE_PIXEL_COLOR, 
 const changePictureRows = rows => ({type: CHANGE_PICTURE_ROWS, rows});
 const changePictureColumns = columns => ({type: CHANGE_PICTURE_COLUMNS, columns});
 const changePicturePixelColor = (x, y, color) => ({type: CHANGE_PICTURE_PIXEL_COLOR, x, y, color});
-const clearerPicture = (columns, rows) => ({type: CLEARER_PICTURE, columns, rows});
+const clearPicture = (columns, rows) => ({type: CLEARER_PICTURE, columns, rows});
 
 export const PICTURE_ACTION = {
     changePictureRows,
     changePictureColumns,
     changePicturePixelColor,
-    clearerPicture
+    clearPicture
 };
